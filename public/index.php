4d02a3f279586312be95d0beccb3bdf4bbb65d64
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-05-12 17:28:43 +0600

    Created Set and Get Method

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
+use App\Core;
+
 include_once '../vendor/autoload.php';
+
+$core = new Core;
+$set = $core->set('name', 'Faiyaz');
+$value = $core->get('name');
---
+++ src/Core.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+class Core
+{
+    protected $data;
+
+    public function __construct()
+    {
+        $this->data = array();
+    }
+
+    public function set($key, $value)
+    {
+        $this->data[$key] = $value;
+    }
+
+    public function get($key)
+    {
+        $keyExists = array_key_exists($key, $this->data);
+        if ($keyExists) {
+            return $this->data[$key];
+        }
+        return null;
+    }
+}
