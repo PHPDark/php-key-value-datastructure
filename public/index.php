fb1f6c601a75fdf3c22979a00a3ed0f12639bc32
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-05-13 23:42:23 +0600

    Created pushListItem Method

--- public/index.php
+++ public/index.php
@@ -5,5 +5,3 @@ use App\Core;
 include_once '../vendor/autoload.php';
 
 $core = new Core;
-$set = $core->set('name', 'Faiyaz');
-$value = $core->get('name');
--- src/Core.php
+++ src/Core.php
@@ -24,4 +24,25 @@ class Core
         }
         return null;
     }
+
+    public function pushListItem($key, $value)
+    {
+        $keyExists = array_key_exists($key, $this->data);
+        if ($keyExists) {
+
+            $retrivedKeyValue = $this->get($key);
+            $result = is_array($retrivedKeyValue) ? array_push($retrivedKeyValue, $value) : false;
+
+            if ($result) {
+                $this->set($key, $retrivedKeyValue);
+                return $retrivedKeyValue;
+            }
+
+        } else {
+            $newArray = array();
+            array_push($newArray, $value);
+            $this->set($key, $newArray);
+            return $newArray;
+        }
+    }
 }
