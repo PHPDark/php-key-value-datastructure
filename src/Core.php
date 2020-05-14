55efff7932f6d468211cd2ee12287269a615d63d
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-05-14 16:05:23 +0600

    Created lpop method

--- src/Core.php
+++ src/Core.php
@@ -67,7 +67,17 @@ class Core
         }
     }
 
-    
+    public function lpop($key)
+    {
+        $keyExists = array_key_exists($key, $this->data);
+        if ($keyExists) {
+            $retrivedKeyValue = $this->get($key);
+            is_array($retrivedKeyValue) ? array_pop($retrivedKeyValue) : false;
+        } else {
+            return false;
+        }
+    }
+
     public function exists($key)
     {
         $keyExists = array_key_exists($key, $this->data);
@@ -83,7 +93,7 @@ class Core
         $keyExists = array_key_exists($key, $this->data);
         if ($keyExists) {
             unset($this->data[$key]);
-        }else{
+        } else {
             return false;
         }
     }
